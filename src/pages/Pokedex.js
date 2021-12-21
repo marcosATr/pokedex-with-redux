256189166d3aec42f24c9291a3e51beb2e421448
marcosATr <marcos.trannin@gmail.com>
2021-12-21 15:41:04 -0300

    first setp into fetching data from api

--- src/pages/Pokedex.js
+++ src/pages/Pokedex.js
@@ -1,7 +1,20 @@
+import { useEffect } from "react";
 import Filter from "../components/filter/Filter";
 import Header from "../components/header/Header";
 
+
+
 export default function Pokedex() {
+  useEffect(()=>{
+
+    async function getTypes() {
+      let response = await fetch('https://pokeapi.co/api/v2/type')
+      response = await response.json()
+      await console.log(response.results)
+    }
+
+    getTypes()
+  }, [])
   return (
     <>
       <Header />
