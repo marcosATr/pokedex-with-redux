60b74feb5226856c1acd12de2f125c23aed2090a
marcosATr <mmarcos.trannin@gmail.com>
2021-12-28 17:29:54 -0300

    404

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { GlobalStyles } from "./styles/GlobalStyles";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Homepage from "./pages/Homepage";
 import Pokedex from "./pages/Pokedex";
+import NotFound from "./pages/NotFound";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Routes>
           <Route exact path="/" element={<Homepage />} />
           <Route exact path="/pokedex" element={<Pokedex />} />
+          <Route path="*" exact element={<NotFound />} />
         </Routes>
         <GlobalStyles />
       </Router>
---
+++ src/pages/NotFound.js
@@ -0,0 +1,19 @@
+import Header from "../components/header/Header";
+
+export default function NotFound() {
+  return (
+    <>
+      <Header />
+      <div>
+        <p>
+          not ffoffoffoffoffoffoffoffoffoffoffoffoffoffoffoffoffoffoffo
+          ffoffoffoffoffoffoffoffo und
+        </p>
+        <p>
+          not ffoffoffoffoffoffoffoffoffoffoffoffoffoffoffoffoffoffoffo
+          ffoffoffoffoffoffoffoffo und
+        </p>
+      </div>
+    </>
+  );
+}
